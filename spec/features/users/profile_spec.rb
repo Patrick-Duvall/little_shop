41177ea39f03cc210dac35a0c86e2281ed62095a
Patrick Duvall <duvallp208@gmail.com>
2019-06-02 11:30:04 -0600

    Add spec for cannot delete addresses associated with completed order

--- spec/features/users/profile_spec.rb
+++ spec/features/users/profile_spec.rb
@@ -289,5 +289,47 @@ RSpec.describe 'user profile', type: :feature do
       end
     end
 
+    describe "addresses with associated orders cant be changed" do
+      it "does not allow me to delete an address associated with a packaged/shipped order" do
+        click_link("Log out")
+        user = create(:user)
+        address = create(:address, user:user)
+        address2 = create(:address, user:user, nick_name: 'school' )
+        address3 = create(:address, user:user, nick_name: 'work' )
+        address4 = create(:address, user:user, nick_name: 'other' )
+
+        pending_order = create(:order, user: user, address: address)
+        pending_order2 = create(:order, user: user, address: address4)
+        packaged_order = create(:packaged_order, user: user, address: address3)
+        shipped_order = create(:shipped_order, user: user, address: address2)
+        cancelled_order = create(:cancelled_order, user: user, address: address4)
+        login_as(user)
+        visit profile_path
+        within "#address-#{address4.id}" do #cancelled can change
+          click_link("Delete #{address4.nick_name}")
+        end
+        save_and_open_page
+        expect(current_path).to eq(profile_path)
+        expect(page).to_not have_css("#address-#{address4.nick_name}")
+        within "#address-#{address3.id}" do #packaged cannot
+          click_link("Delete #{address3.nick_name}")
+        end
+        expect(current_path).to eq(profile_path)
+        expect(page).to have_content("Cannot Delete #{address3.nick_name}, it is associated with a packaged or shipped order")
+        expect(page).to have_css("#address-#{address3.nick_name}")
+        within "#address-#{address2.id}" do #shipped canot
+          click_link("Delete #{address2.nick_name}")
+        end
+        expect(page).to have_content("Cannot Delete #{address2.nick_name}, it is associated with a packaged or shipped order")
+        expect(page).to have_css("#address-#{address2.nick_name}")
+        within "#address-#{address.id}" do #pending can
+          click_link("Delete #{address.nick_name}")
+        end
+        expect(page).to_not have_content("Cannot Delete #{address.nick_name}, it is associated with a packaged or shipped order")
+        expect(page).to_not have_css("#address-#{address.nick_name}")
+
+      end
+    end
+
   end
 end
