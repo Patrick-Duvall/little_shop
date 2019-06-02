15fb1a4a76e938a257c9d942d0d11acd088159c5
Patrick Duvall <duvallp208@gmail.com>
2019-06-02 11:30:04 -0600

    Add addresses variable to admin users show controller to not error on new view

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -5,6 +5,7 @@ class Admin::UsersController < Admin::BaseController
 
   def show
     @user = User.find(params[:id])
+    @addresses = @user.addresses
     if @user.role == "merchant"
       redirect_to admin_merchant_path(@user)
     end
